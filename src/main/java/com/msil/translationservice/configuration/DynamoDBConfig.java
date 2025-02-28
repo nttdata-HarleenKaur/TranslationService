53b73297ea9bb866c148735f0df20ea1fd7aee48
nttdata-HarleenKaur <nttdata_harleen.kaur@maruti.co.in>
2025-02-28 15:23:41 +0530

    Initial commit

---
+++ src/main/java/com/msil/translationservice/configuration/DynamoDBConfig.java
@@ -0,0 +1,8 @@
+package com.msil.translationservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+
+}
---
+++ src/main/java/com/msil/translationservice/controller/TranslationController.java
@@ -0,0 +1,8 @@
+package com.msil.translationservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/v1/")
+public class TranslationController {
+
+}
